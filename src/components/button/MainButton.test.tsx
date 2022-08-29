eb1009c31487fb770139497f1751d8d65dfe92f9
scava <sorzesimone1@gmail.com>
2022-08-29 22:02:52 +0200

    added button component - changed label

--- src/components/button/MainButton.test.tsx
+++ src/components/button/MainButton.test.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import MainButton from './MainButton';
 
-test('renders learn react link', () => {
+test('renders <MainButton>', () => {
 	render(<MainButton />);
 	const element = screen.getByRole('button');
 	expect(element).toBeInTheDocument();
